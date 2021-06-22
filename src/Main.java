a82f7571ae0138aadf4316440d8832e34ad2d6f2
Kirill <Petrov.Kirill.dev@gmail.com>
2021-06-22 15:44:06 +0400

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+
+    }
+}
